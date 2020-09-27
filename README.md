64371429c4d018143d188c90e6a2d7155e7193db
bondhan bondhan <bondhan@sinbad.co.id>
2020-09-27 15:28:41 +0700

    first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# covidbondhan
-Tracking Covid App 2020 (React Native)
---
+++ src/screens/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Home 1</Text>
+      <Text>Home 2</Text>
+    </View>
+  );
+}
