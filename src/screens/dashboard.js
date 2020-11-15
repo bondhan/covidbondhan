4b5770206ee9a54b83b005592ff7c7db70e92a83
bondhan bondhan <bondhan@sinbad.co.id>
2020-11-15 20:54:02 +0700

    using correct useEffect and render

--- src/screens/dashboard.js
+++ src/screens/dashboard.js
@@ -1,14 +1,14 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, Button, TouchableOpacity} from 'react-native';
+import {SafeAreaView, FlatList, Text, TouchableOpacity} from 'react-native';
 import CovidIndonesiaApi from '../api/covidIndonesia';
 
 export default function Dashboard({navigation}) {
 
     const scv = [
-        {id: 'Jumlah Kasus', value: 0},
-        {id: 'Meninggal', value: 0},
-        {id: 'Sembuh', value: 0},
-        {id: 'Perawatan', value: 0},
+        {id: 1, type: 'Jumlah Kasus', value: 0},
+        {id: 2, type: 'Meninggal', value: 0},
+        {id: 3, type: 'Sembuh', value: 0},
+        {id: 4, type: 'Perawatan', value: 0},
     ];
 
     const [summaryCovidData, setSummaryCovid] = useState([scv]);
@@ -16,24 +16,24 @@ export default function Dashboard({navigation}) {
     useEffect(() => {
         async function getCovidData() {
             try {
-                let response = await CovidIndonesiaApi.get('/');
+                const response = await CovidIndonesiaApi.get('/');
                 let sc = [
-                    {id: 'Jumlah Kasus', value: response.data.jumlahKasus},
-                    {id: 'Meninggal', value: response.data.meninggal},
-                    {id: 'Sembuh', value: response.data.sembuh},
-                    {id: 'Perawatan', value: response.data.perawatan},
+                    {id: 1, type: 'Jumlah Kasus', value: response.data.jumlahKasus},
+                    {id: 2, type: 'Meninggal', value: response.data.meninggal},
+                    {id: 3, type: 'Sembuh', value: response.data.sembuh},
+                    {id: 4, type: 'Perawatan', value: response.data.perawatan},
                 ];
 
                 setSummaryCovid(sc);
+
             } catch (error) {
                 console.log(error);
             }
         }
 
         getCovidData();
-        // console.log(summaryCovidData);
 
-    }, [summaryCovidData]);
+    }, []);
 
     function JSONEmpty(obj) {
         return !obj.length ||
@@ -43,27 +43,21 @@ export default function Dashboard({navigation}) {
     }
 
     return (
-        <View>
-            <Text>
-            {
-
-                summaryCovidData != undefined && !JSONEmpty(summaryCovidData) ?
-                    summaryCovidData.map(
-                        (dataCovid) => {
-                            return(
-                            <TouchableOpacity>
-                                <Text onPress={() => navigation.navigate('Dashboard Details')}>
-                                    {"\n"}
-                                    {dataCovid.id} : {dataCovid.value}
-                                    {"\n"}
-                                </Text>
-                            </TouchableOpacity>
-                            )
-                        },
-                    )
-                    : 'N/A'
-            }
-            </Text>
-        </View>
+        <SafeAreaView>
+            <FlatList
+                data={summaryCovidData}
+                renderItem={({item}) => {
+                    return (<TouchableOpacity>
+                        <Text onPress={() => navigation.navigate('Dashboard Details')}>
+                            {'\n'}
+                            {item.type} : {item.value}
+                            {'\n'}
+                        </Text>
+                    </TouchableOpacity>);
+                }}
+                keyExtractor={summaryCovidData => summaryCovidData.id}
+            />
+        </SafeAreaView>
     );
 }
+
